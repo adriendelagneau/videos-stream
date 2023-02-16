11b8493999c88c1f77b3ffd35b650436106fa969
adriendelagneau <delagneauadrien@yahoo.fr>
2023-02-17 00:15:49 +0100

    get data

--- pages/videos.js
+++ pages/videos.js
@@ -1,3 +1,4 @@
+import Sidebar from '@/components/Sidebar';
 import VideoCard from '@/components/VideoCard';
 import axios from 'axios';
 import React from 'react';
@@ -5,13 +6,13 @@ import React from 'react';
 const Videos = (props) => {
     return (
       <>
-      <div className='w-[100%]  dark:bg-myBlack dark:text-white h-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 place-items-center'>
-           
+       <div className='flex w-full bg-myWhite dark:bg-myBlack'>
+      <Sidebar />
+      <div className='w-[100%]  dark:bg-myBlack dark:text-white h-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4  mt-[100px] ml-[62px] sm:ml-[170px] min-h-[calc(100vh-100px)]'>
         {props.posts?.map((post, i) => (
-          
           <VideoCard key={i} post={post} />
         ))}
-           
+        </div>   
         </div>
     </>
     );
@@ -25,7 +26,7 @@ export async function getServerSideProps({query}) {
     const search = query.search || 'all'
   
     const res = await axios.get(
-      `{${process.env.NEXTAUTH_URL}/api/post?topic=${topic}&title=${search}`
+      `${process.env.NEXTAUTH_URL}/api/post?topic=${topic}&title=${search}`
     )
 
     return {
